41d9130b1a7571217e0c1498436cab6c42f7b8c4
Dikizeko-Oliveira <dikezekodagraca04@gmail.com>
2019-09-09 21:09:33 -0300

    It's just a simple project with JS

---
+++ js/myScript.js
@@ -0,0 +1,44 @@
+var btnMenu =  document.getElementById("btn-menu");
+
+btnMenu.addEventListener("click", toggleMenu);
+
+function toggleMenu(e){
+
+    var divOverlay = document.getElementsByClassName("overlay")[0];
+    var speed = 10;
+
+    var eventTarget =  e.target;
+
+    if(eventTarget.className == "btn-open"){
+        fadeIn(divOverlay, speed);
+        eventTarget.className = "btn-close";
+    }
+    else if(eventTarget.className == "btn-close"){
+        fadeOut(divOverlay, speed);
+        eventTarget.className = "btn-open";
+    }
+
+}
+
+function fadeIn(element, speed){
+    var inInterval = setInterval(function(){
+            element.style.opacity = Number(element.style.opacity) + 0.02;
+
+            if(element.style.opacity >= 1){
+                element.style.opacity = 1;
+                clearInterval(inInterval);
+            }
+    }, speed);
+}
+
+function fadeOut(element, speed){
+    var ouInterval = setInterval(function(){
+            element.style.opacity = Number(element.style.opacity) - 0.02;
+
+            if(element.style.opacity <= 1){
+                element.style.opacity = 0;
+                clearInterval(ouInterval);
+            }
+    }, speed);
+}
+
